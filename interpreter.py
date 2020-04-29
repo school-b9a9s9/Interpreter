76c6a7ab02c4fcd9145f163c54d02d302732d717
Bas <bas.vaneck@ziggo.nl>
2020-04-29 19:04:49 +0200

    begin of parser, can nou parse addition sums (e.q. 5+4-3)

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple, Callable, Union
+import re
 
 # Reading input file
 inputFile = open("input.txt", "r")
@@ -7,8 +8,17 @@ sourceCode= inputFile.read()
 # ---------------------------------------------
 # Classes
 # ---------------------------------------------
+class Error():
+    def __init__(self, errorMessage: str, line: int, position: int):
+        self.errorMessage = errorMessage
+        self.line = line
+        self.position = position
+
+    def __repr__(self):
+        return self.errorMessage + 'at line: ' + str(self.line) + 'position: ' + str(self.position)
+
 class Token():
-    def __init__(self, type, value, line, position):
+    def __init__(self, type: str, value: str, line: int, position: int):
         self.type = type
         self.value = value
         self.line = line
@@ -21,6 +31,28 @@ class Token():
 # ---------------------------------------------
 # Lexer
 # ---------------------------------------------
+
+def getTokenType(value: str) -> str:
+    tokenTypes = {
+        'ADD'               : r'[+]',
+        'MULTIPLY'          : r'[*]',
+        'DIVIDE'            : r'[/]',
+        'SUBTRACT'          : r'[-]',
+        'ASSIGN/COMPARE'    : r'[=]',
+        'BLOCK'             : r'[()]',
+        'END'               : r';',
+        'IF'                : r'^if$',
+        'NUMBER'            : r'^[0-9]*$',
+        'IDENTIFIER'        : r'\w'             # Has to be last
+    }
+    for tokenType in tokenTypes:
+        if re.match(tokenTypes.get(tokenType), value):
+            return tokenType
+
+    return "UNKNOWN"
+
+
+# --------Vraag: moet ik in deze functie definitie nou iets zeggen over getTokenType of niet?-------
 # createToken :: str, Tuple[int,int] -> Callable[List[Token]] -> Union[List[Token], None]
 def createToken(value: str, position: Tuple[int, int]) -> Union[Callable[[List[Token]], List[Token]], Callable[[List[Token]], None]]:
     def addTokenToList(tokenList):
@@ -32,18 +64,18 @@ def createToken(value: str, position: Tuple[int, int]) -> Union[Callable[[List[T
     if len(value) == 0:
         return doNothing
     elif not str.isdigit(value) and not str.isalpha(value):
-        token =  Token('TODO', value, position[0], position[1])
+        token = Token(getTokenType(value), value, position[0], position[1])
     else:
         startPosition = (position[0], position[1] - len(value))
-        token = Token('TODO', value, startPosition[0], startPosition[1])
+        token = Token(getTokenType(value), value, startPosition[0], startPosition[1])
     return addTokenToList
 
-def updateCurrentPosition(currentCharacter):
-    def newLine(lastPosition: List[int]):
+def updateCurrentPosition(currentCharacter) -> Callable[[Tuple[int, int]], Tuple[int, int]]:
+    def newLine(lastPosition: Tuple[int, int]) -> Tuple[int, int]:
         newPosition = lastPosition[0] + 1, 1
         return newPosition
 
-    def nextCharacter(lastPosition: List[int]):
+    def nextCharacter(lastPosition: Tuple[int, int]) -> Tuple[int, int]:
         newPosition = lastPosition[0], lastPosition[1] + 1
         return newPosition
 
@@ -53,7 +85,7 @@ def updateCurrentPosition(currentCharacter):
         return nextCharacter
 
 # lex :: str -> List[Token]
-def lex(sourceCode: str, s: str = '', currentPosition: Tuple[int, int] = (1, 1))->List[Token]: # sourceCode: code, s: not-tokens to be converted a token
+def lex(sourceCode: str, s: str = '', currentPosition: Tuple[int, int] = (1, 1)) -> List[Token]: # sourceCode: code, s: not-tokens to be converted a token
     head, *tail = sourceCode
     tokens: List(Token) = []
 
@@ -79,9 +111,79 @@ def lex(sourceCode: str, s: str = '', currentPosition: Tuple[int, int] = (1, 1))
         return tokens
 
 
+# ---------------------------------------------
+# Parser
+# ---------------------------------------------
+PRECEDENCE = {
+    '=' : 1,
+    '<' : 2,
+    '>' : 2,
+    '+' : 3,
+    '-' : 3,
+    '*' : 4,
+    '/' : 4
+}
+
+class AST():
+    pass
+
+class Statement(AST):
+    def __init__(self, expressions):
+        self.expressions = expressions
+
+class Block(AST):
+    def __init__(self, statements: Statement):
+        self.statements = statements
+
+class Number(AST):
+    def __init__(self, value: Token):
+        self.value = value
+    def __repr__(self):
+        return 'Number(' + str(self.value.value) + ')'
+
+class BinaryOperator(AST):
+    def __init__(self, left: AST, operator: Token, right: AST):
+        self.left = left
+        self.operator = operator
+        self.right = right
+    def __repr__(self):
+        return 'BinaryOperator(' + str(self.left) + ', Operator(' + str(self.operator.value) + '), ' + str(self.right) + ')'
+
+def parseBinaryOperator(lhs: AST,tokenList: List[Token]):
+    # tokenlist is tokens after the +
+    head, *tail = tokenList
+    if tail[0].type != 'NUMBER' and tail[0].type != 'IDENTIFIER' and tail[0].type != 'BLOCK':
+        return Error('Invalid syntax ', head.line, head.position)
+    elif tail[1].type == 'END':
+        return BinaryOperator(lhs, head, Number(tail[0]))
+    elif tail[1].type != 'ADD' and tail[1].type != 'SUBTRACT' and tail[1].type != 'MULTIPLY' and tail[1].type != 'DIVIDE':
+        return Error('Invalid syntax ', head.line, head.position)
+    else:
+        return BinaryOperator(lhs, head, parseGeneral(tail))
+
+def parseGeneral(tokenList: List[Token], prev: List[AST] = []):
+    head, *tail = tokenList
+
+    if head.type == 'NUMBER':
+        prev.append(Number(head))
+        return parseGeneral(tail, prev)
+
+    elif head.type == 'ADD' or head.type == 'SUBTRACT':
+        lhs = prev.pop()
+        return parseBinaryOperator(lhs, tokenList)
+
+
+    elif head.type == 'END':
+        return prev.append(parseGeneral(tail, prev))
+
+
+def parse(tokenList: List[Token], prev = None):
+    return parseGeneral(tokenList)
 
 # ---------------------------------------------
 # Run/Debug
 # ---------------------------------------------
-for i in (lex(sourceCode)):
-    print(i)
+# for i in (lex(sourceCode)):
+#     print(i)
+
+print(parse(lex(sourceCode)))
